b771db6433cc979d247e06f1c0999d67faaa2081
bahyuhh <bbbbbbbahyu701@gmail.com>
2024-01-22 09:36:41 +0700

    done to pwa

--- public/script.js
+++ public/script.js
@@ -27,3 +27,27 @@ buttons.forEach((button) => {
   //Button click listener calls calculate() with dataset value as argument.
   button.addEventListener("click", (e) => calculate(e.target.dataset.value));
 });
+
+// service-worker.js
+
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+  '/',
+  'style.css',
+  'script.js',
+  '/calculator.ico'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
